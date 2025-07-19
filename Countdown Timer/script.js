3b9be437d9159577f5ce88bbb4c1161f8bf26b70
ayushporwal01 <porwalayush953@gmail.com>
2025-07-20 00:03:37 +0530

    Initial Commit

---
+++ Countdown Timer/script.js
@@ -0,0 +1,9 @@
+let countdown;
+let isPausee = false;
+let remainingTime = 0;
+
+const timer = document.getElementById("timer");
+const startBtn = document.getElementById("start");
+const pause = document.getElementById("pause");
+const reset = document.getElementById("reset");
+
