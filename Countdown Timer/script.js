f764059df1e194a5cfaa53f238da211a123cd3e2
ayushporwal01 <porwalayush953@gmail.com>
2025-07-20 09:47:48 +0530

    Initial Commit

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -8,5 +8,7 @@ const pause = document.getElementById("pause");
 const reset = document.getElementById("reset");
 
 function updateTime(seconds) {
- const hrs = String(Math.floor(seconds / 3600))
+ const hrs = String(Math.floor(seconds / 3600)).padStart(2, "0"); //get full hours
+ const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0"); //get remaining minutes
+ const secs = String(seconds % 60).padStart(2, "0"); //gives remaining seconds 
 }
