171f3c28ff8ecbeabdd95a65a6ddd8a5e6a78fff
ayushporwal01 <porwalayush953@gmail.com>
2025-07-20 09:50:17 +0530

    Initial Commit

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -2,7 +2,7 @@ let countdown;
 let isPausee = false;
 let remainingTime = 0;
 
-const timer = document.getElementById("timer");
+const timerDisplay = document.getElementById("timer");
 const startBtn = document.getElementById("start");
 const pause = document.getElementById("pause");
 const reset = document.getElementById("reset");
@@ -11,4 +11,6 @@ function updateTime(seconds) {
  const hrs = String(Math.floor(seconds / 3600)).padStart(2, "0"); //get full hours
  const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0"); //get remaining minutes
  const secs = String(seconds % 60).padStart(2, "0"); //gives remaining seconds 
+ timerDisplay.textContent= `${hrs}:${mins}:${secs}`; //updates timer in ui
 }
+
