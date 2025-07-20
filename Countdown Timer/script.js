1e4842fa4620ad7bfa4189406576d0a6435ff472
ayushporwal01 <porwalayush953@gmail.com>
2025-07-20 10:07:39 +0530

    Initial Commit

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -8,15 +8,16 @@ const pause = document.getElementById("pause");
 const reset = document.getElementById("reset");
 
 function updateTime(seconds) {
- const hrs = String(Math.floor(seconds / 3600)).padStart(2, "0"); //get full hours
- const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0"); //get remaining minutes
- const secs = String(seconds % 60).padStart(2, "0"); //gives remaining seconds 
- timerDisplay.textContent= `${hrs}:${mins}:${secs}`; //updates timer in ui
+  const hrs = String(Math.floor(seconds / 3600)).padStart(2, "0"); //get full hours
+  const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0"); //get remaining minutes
+  const secs = String(seconds % 60).padStart(2, "0"); //gives remaining seconds
+  timerDisplay.textContent = `${hrs}:${mins}:${secs}`; //updates timer in ui
 }
 
 startBtn.addEventListener("click", () => {
-    if(countdown || isPaused) return; 
-    
-   const hrs = parseInt(document.getElementById("hours").value) || 0; //gets input value, converts it to number, invalid value default to 0, prints it
-   
-})
+  if (countdown || isPaused) return;
+
+  const hrs = parseInt(document.getElementById("hours").value) || 0; //gets input value, converts it to number, invalid value default to 0, prints it
+  const mins = parseInt(document.getElementById("minutes").value) || 0;
+  const secs = parseInt(document.getElementById("seconds").value) || 0;
+});
