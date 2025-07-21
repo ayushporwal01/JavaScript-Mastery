1cc7d62087385fe7e17ae00c096b220239a7fdee
ayushporwal01 <porwalayush953@gmail.com>
2025-07-21 23:01:30 +0530

    Initial Commit

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,3 +1,7 @@
 let startTime = 0;
 let interval = null;
 let running = false;
+
+const display = document.querySelector(".display");
+const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
